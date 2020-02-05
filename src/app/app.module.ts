2571391a9f36a9de252c4d29e0881aa0a1a93e35
Matt <matt@frikz.co>
2020-02-05 08:30:21 +0000

    added routing + product detail component to application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,36 @@ import { BrowserModule }    from '@angular/platform-browser';
 import { NgModule }         from '@angular/core';
 import { FormsModule }      from '@angular/forms';
 import { HttpClientModule}  from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }         from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { StarComponent }        from './shared/star.component';
 
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     StarComponent,
-    ConvertToSpacesPipe
+    ConvertToSpacesPipe,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'product', component: ProductListComponent },
+      { path: 'product/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])
   ],
   bootstrap: [
     AppComponent
