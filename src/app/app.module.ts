a22aa8e668243a14bd3b3213cfd0e8260edeb210
Matt <matt@frikz.co>
2020-02-28 18:53:17 +0000

    add routing modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule }    from '@angular/platform-browser';
 import { NgModule }         from '@angular/core';
 import { HttpClientModule}  from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 import { ProductModule } from './products/product.module';
 
 import { AppComponent }         from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -16,15 +16,9 @@ import { WelcomeComponent } from './home/welcome.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      { path: 'welcome', component: WelcomeComponent },
-      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-    ]),
-    ProductModule
+    ProductModule,
+    AppRoutingModule
   ],
-  bootstrap: [
-    AppComponent
-  ]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,23 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { ProductRoutingModule } from '../products/product-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 
-import { ProductDetailGuard } from './product-detail.guard';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
-    RouterModule.forChild([
-      { path: 'products', component: ProductListComponent },
-      { 
-        path: 'products/:id', 
-        canActivate: [ProductDetailGuard],
-        component: ProductDetailComponent 
-      }
-    ]),
+    ProductRoutingModule,
     SharedModule
   ],
   declarations: [
